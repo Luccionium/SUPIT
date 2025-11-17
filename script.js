078213c04433cf95a690133c73d365716203e5bd
Lucconium <luka.cucuk554@gmail.com>
2025-11-17 13:46:23 +0100

    dodan kontakt popup form

---
+++ script.js
@@ -0,0 +1,10 @@
+const openBtn = document.getElementById("openmodal");
+const closeBtn = document.getElementById("closemodal");
+const modal = document.getElementById("modal");
+
+openBtn.addEventListener("click", () => {
+    modal.classList.add("open");
+})
+closeBtn.addEventListener("click", () => {
+    modal.classList.remove("open");
+})
