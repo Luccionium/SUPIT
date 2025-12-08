dade6fb2d365834f12fce80f1da545717b960cd0
Luccionium <luka.cucuk554@gmail.com>
2025-12-08 17:58:09 +0100

    dodan login/register modal, nije finuto

--- script.js
+++ script.js
@@ -1,12 +1,51 @@
-const openBtn = document.getElementById("openmodal");
-const closeBtn = document.getElementById("closemodal");
-const modal = document.getElementById("modal");
+const openContactBtn = document.getElementById("openmodal");
+const closeContactBtn = document.getElementById("closemodal");
+const contactModal = document.getElementById("modal");
 
-openBtn.addEventListener("click", () => {
-    modal.classList.add("open");
-})
-closeBtn.addEventListener("click", () => {
-    modal.classList.remove("open");
-})
+openContactBtn.addEventListener("click", () => {
+    contactModal.classList.add("open");
+});
 
-const notifications = document.getElementById("notifications").checked;
+closeContactBtn.addEventListener("click", () => {
+    contactModal.classList.remove("open");
+});
+
+
+
+const openLoginBtn = document.getElementById("open-login-modal");
+const closeLoginBtn = document.getElementById("close-login-modal");
+const loginModal = document.getElementById("login-modal");
+
+openLoginBtn.addEventListener("click", () => {
+    loginModal.classList.add("open");
+});
+
+closeLoginBtn.addEventListener("click", () => {
+    loginModal.classList.remove("open");
+});
+
+
+
+
+const tabLogin = document.getElementById("tab-login");
+const tabRegister = document.getElementById("tab-register");
+
+const formLogin = document.getElementById("loginForm");
+const formRegister = document.getElementById("registerForm");
+
+tabLogin.addEventListener("click", () => {
+  tabLogin.classList.add("active");
+  tabRegister.classList.remove("active");
+
+  formLogin.classList.add("active");
+  formRegister.classList.remove("active");
+});
+
+tabRegister.addEventListener("click", () => {
+  tabRegister.classList.add("active");
+  tabLogin.classList.remove("active");
+
+  formRegister.classList.add("active");
+  formLogin.classList.remove("active");
+});
+
