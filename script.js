e919a61decdfb743ff26c7b1cd7c40892e176f90
Luccionium <luka.cucuk554@gmail.com>
2025-12-15 09:02:08 +0100

    završena prijava, treba dodat nastavni plan

--- script.js
+++ script.js
@@ -49,3 +49,62 @@ tabRegister.addEventListener("click", () => {
   formLogin.classList.remove("active");
 });
 
+//Login alertovi i odjava
+
+function toggleLoginLogout() {
+  const loginBtn = document.getElementById("open-login-modal");
+  const loginIcon = loginBtn.querySelector(".login-icon");
+  const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
+  
+  if (isLoggedIn) {
+    loginIcon.classList.add("flipped");
+    
+    loginBtn.innerHTML = '<i class="material-symbols-outlined login-icon flipped">login</i>Odjava';
+    
+    loginBtn.onclick = (e) => {
+      e.preventDefault();
+      localStorage.removeItem("isLoggedIn");
+      localStorage.removeItem("username");
+      toggleLoginLogout();
+      alert("Uspješno ste se odjavili!");
+    };
+  } else {
+    loginBtn.innerHTML = '<i class="material-symbols-outlined login-icon">login</i>Prijava';
+    
+    loginBtn.onclick = () => {
+      loginModal.classList.add("open");
+    };
+  }
+}
+
+formLogin.addEventListener("submit", (e) => {
+  e.preventDefault();
+  
+  const username = document.getElementById("login-username").value;
+  
+  localStorage.setItem("isLoggedIn", "true");
+  localStorage.setItem("username", username);
+  
+  loginModal.classList.remove("open");
+  toggleLoginLogout();
+  alert("Uspješno ste se prijavili!");
+  
+  formLogin.reset();
+});
+
+formRegister.addEventListener("submit", (e) => {
+  e.preventDefault();
+  
+  const username = document.getElementById("reg-username").value;
+  
+  localStorage.setItem("isLoggedIn", "true");
+  localStorage.setItem("username", username);
+  
+  loginModal.classList.remove("open");
+  toggleLoginLogout();
+  alert("Uspješno ste se registrirali i prijavili!");
+  
+  formRegister.reset();
+});
+
+toggleLoginLogout();
